cffa2c5ace5b929d18355b085c9c29c20e3db36c
Abdelhakim ER-RAFAIY <a.errafaiy@void.fr>
2020-03-22 14:20:15 +0100

    covid-19

---
+++ asset/js/data.js
@@ -0,0 +1,21 @@
+const api_url = 'https://corona.lmao.ninja/countries/morocco';
+	
+async function getData() {
+
+    const response = await fetch(api_url);
+    const data = await response.json();
+
+    $(".tConfirmed").text(data.cases);
+    $(".tActive").text(data.active);
+    $(".tDeaths").text(data.deaths);
+    $(".tRecovered").text(data.recovered);
+    $(".tCritical").text(data.critical);
+    $('.tTodayCases').text(data.todayCases);
+    $('.tTodayDeaths').text(data.todayDeaths);
+    $('.tPersontage').text(Math.round(((data.deaths * 100) / data.cases))  + "%");
+
+        }
+
+        getData();
+
+        
