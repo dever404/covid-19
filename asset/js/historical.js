4d0a1a0ed1860062513c631a9ede2f3550f33501
errafaiy <abdo.errafaiy@gmail.com>
2020-07-24 12:45:37 +0100

    Update historical.js

--- asset/js/historical.js
+++ asset/js/historical.js
@@ -42,7 +42,7 @@ async function getHestorical() {
     };
 
     //var chart = new google.visualization.ColumnChart(document.getElementById('linechart_material'));
-    var chart = new google.visualization.LineChart(document.getElementById('linechart_material'));
+    chart = new google.visualization.LineChart(document.getElementById('linechart_material'));
     chart.draw(chartdata, options); 
 
     //chart.draw(chartdata, google.charts.Line.convertOptions(options));
